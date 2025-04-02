cb9873ff1fa60c13070edcdc23557b316d95e3eb
Harsh Murjani <103986690+hm05@users.noreply.github.com>
2025-04-02 17:37:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check if the script is run as root
+# Check if the script is being run as root
 if [ "$(whoami)" != "root" ]; then
     echo "Please run the script as root user"
     exit 1
@@ -12,16 +12,21 @@ apt -y upgrade
 apt -y full-upgrade
 apt -y autoremove
 
-# Install Armitage and start PostgreSQL service
+# Install gedit
+apt install -y gedit
+
+# Install Armitage and initialize PostgreSQL database for Metasploit
 apt install -y armitage
 /etc/init.d/postgresql start
-
-# Initialize Metasploit database
 msfdb init
 
-# Download and install Obsidian
-curl -L -o obsidian_1.8.9_amd64.deb https://github.com/obsidianmd/obsidian-releases/releases/download/v1.8.9/obsidian_1.8.9_amd64.deb
-dpkg -i obsidian_1.8.9_amd64.deb
+# Check system architecture and download appropriate Obsidian version
+if [ "$(uname -m)" == "aarch64" ]; then
+    curl -L -o Obsidian-1.8.9-arm64.AppImage https://github.com/obsidianmd/obsidian-releases/releases/download/v1.8.9/Obsidian-1.8.9-arm64.AppImage
+    chmod u+x Obsidian-1.8.9-arm64.AppImage
+else
+    curl -L -o Obsidian-1.8.9.AppImage https://github.com/obsidianmd/obsidian-releases/releases/download/v1.8.9/Obsidian-1.8.9.AppImage
+    chmod u+x Obsidian-1.8.9.AppImage
+fi
 
-# Final message
 echo "Your system is ready to use"
